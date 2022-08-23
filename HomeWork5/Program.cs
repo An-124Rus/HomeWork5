f3f757e8aea4304dcca111047b439c748269dfaa
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-08-23 11:50:36 +0300

    HomeWork5

---
+++ HomeWork5/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name = "Alex";
+        string surname = "Ivanov";
+        string newName;
+        string newSurname;
+
+        Console.WriteLine(name + surname);
+        Console.ReadKey();
+
+        newName = surname;
+        newSurname = name;
+
+        Console.Write(newName + newSurname);
+    }
+}
